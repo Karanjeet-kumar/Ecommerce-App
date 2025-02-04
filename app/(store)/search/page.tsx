f57c9c5bc3e6bc11d2d9bf5b34836ea1eade62ca
Karanjeet-kumar <karanrajput8400@gmail.com>
2025-02-05 00:39:41 +0530

    Creating SearchPage

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+async function searchPage({
+    searchParams,
+}: {
+    searchParams: {
+        query: string;
+    };
+}) {
+    const { query } = await searchParams;
+    return (
+        <div>searchPage for {query}</div>
+    )
+}
+
+export default searchPage
